c68a553e57a710d2b0f73be55db30855c51ad89b
Constantin111 <hrom1771@gmail.com>
2020-05-12 20:15:45 +0300

    Первый коммит

---
+++ src/components/CompOne.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="df-wrap">
+        <h2 class="df-h2">Первый компонент</h2>
+        <p class="df-prg"></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CompOne"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CompTwo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="df-wrap">
+        <h2 class="df-h2">Второй компонент</h2>
+        <p class="df-prg"></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CompTwo"
+    }
+</script>
+
+<style scoped>
+
+</style>
